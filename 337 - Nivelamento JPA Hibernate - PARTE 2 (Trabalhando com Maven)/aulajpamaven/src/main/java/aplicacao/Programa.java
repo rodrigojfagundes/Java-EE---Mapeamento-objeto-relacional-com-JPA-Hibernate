93b92d61abbdf7ca384149a07b8445e1054717a5
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-07 16:39:38 -0300

    Nivelamento JPA Hibernate - Parte 2
    Aula 337.

---
+++ 337 - Nivelamento JPA Hibernate - PARTE 2 (Trabalhando com Maven)/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,25 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+//criando a classe programa
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		//instanciando um objeto do TIPO PESSOA... Ou seja estamos chamando a CLASSE
+		//Pessoa e passando alguns valores para os ATRIBUTOS/VARIAVEIS ID, NOME, EMAIL
+		//e chamando esse OBJETO do tipo PESSOA de P1...	P2 e P3 tbm são
+		
+		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
+		
+		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
+		
+		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+	}
+
+}
